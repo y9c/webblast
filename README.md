4c94013e9e3a054e79f41448b92482ae86a95bec
Chang Ye <yech1990@gmail.com>
2024-06-29 01:05:05 -0500

    quick update

---
+++ README.md
@@ -0,0 +1,4 @@
+# web-blast
+
+[![Pypi Releases](https://img.shields.io/pypi/v/webblast.svg)](https://pypi.python.org/pypi/webblast)
+[![Downloads](https://pepy.tech/badge/webblast)](https://pepy.tech/project/webblast)
--- webblast/utils.py
+++ webblast/utils.py
@@ -7,49 +7,55 @@
 # Created: 2024-06-29 00:18
 
 
-import os
 import gzip
+import os
+
 import pysam
+
 
 def read_file(file_path, record_limit):
     """Reads files depending on their format, handling gzipped files and returning sequences in FASTA format without annotations."""
     _, file_ext = os.path.splitext(file_path)
     sequences = []
-    
+
     # Decide on the method to open the file based on its compression
     def open_file(file_path):
-        if file_path.endswith('.gz'):
-            return gzip.open(file_path, 'rt')
+        if file_path.endswith(".gz"):
+            return gzip.open(file_path, "rt")
         else:
-            return open(file_path, 'rt')
-    
+            return open(file_path, "rt")
+
     # Processing the file based on its extension
     with open_file(file_path) as file:
-        if file_ext in ['.fasta', '.fa', '.fasta.gz', '.fa.gz']:
+        if file_ext in [".fasta", ".fa", ".fasta.gz", ".fa.gz"]:
             name, seq = None, []
             for line in file:
-                if line.startswith('>'):
+                if line.startswith(">"):
                     if name:
                         sequences.append(f">{name}\n{''.join(seq)}")
                         if len(sequences) >= record_limit:
                             break
-                    name = line[1:].split(None, 1)[0].strip()  # Take the first part of the header
+                    name = (
+                        line[1:].split(None, 1)[0].strip()
+                    )  # Take the first part of the header
                     seq = []
                 else:
                     seq.append(line.strip())
             if name and len(sequences) < record_limit:
                 sequences.append(f">{name}\n{''.join(seq)}")
-        elif file_ext in ['.fastq', '.fq', '.fastq.gz', '.fq.gz']:
+        elif file_ext in [".fastq", ".fq", ".fastq.gz", ".fq.gz"]:
             counter = 0
             for line in file:
                 if counter % 4 == 0:  # Name line
-                    name = line[1:].strip().split()[0]  # Remove '@' and take the first part
+                    name = (
+                        line[1:].strip().split()[0]
+                    )  # Remove '@' and take the first part
                 elif counter % 4 == 1:  # Sequence line
                     sequences.append(f">{name}\n{line.strip()}")
                     if len(sequences) >= record_limit:
                         break
                 counter += 1
-        elif file_ext in ['.sam', '.bam']:
+        elif file_ext in [".sam", ".bam"]:
             with pysam.AlignmentFile(file_path, "r") as samfile:
                 for aln in samfile:
                     if aln.query_sequence:
@@ -58,4 +64,3 @@ def read_file(file_path, record_limit):
                             break
 
     return sequences
-
